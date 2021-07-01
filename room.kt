1c826751f7e0336db559e1667cd69a62d458ce68
“kkiirr201314” <qq255073@gmail.com>
2021-07-01 10:48:01 +0800

    c14-5

--- room.kt
+++ room.kt
@@ -3,4 +3,6 @@ open class room(val name: String){
 
     fun load () = "這裡沒什麼可看的..."
 }
-class TownSquare : room("Town Square")
+class TownSquare : room("Town Square"){
+    override fun  load() = "因為你的到來村民正在歡呼與集會"
+}
